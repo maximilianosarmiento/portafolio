2c36a8080763a2726c8b1797f02e8ef200a2b919
MAXIMILIANO_SARMIENTO <maximiliano.sarmiento11@gmail.com>
2022-10-28 13:34:17 -0300

    animacion de foto de perfil

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,48 +2,52 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
 
-  darkMode:'class',
-  
+  darkMode: "class",
+
   theme: {
     extend: {
       colors: {
-        naranja: '#EC994B',
-          grisClaro: '#F1EEE9',
-          grisOscuro: '#73777B',
-          azul: '#15133C'
+        naranja: "#EC994B",
+        grisClaro: "#F1EEE9",
+        grisOscuro: "#73777B",
+        azul: "#15133C",
       },
 
-      fontFamily:{
-        kodchasan: 'kodchasan',
+      fontFamily: {
+        kodchasan: "kodchasan",
       },
-    
 
-        backdropFilter: {
-          'none': 'none',
-          'blur': 'blur(0px)',
+      backdropFilter: {
+        none: "none",
+        blur: "blur(0px)",
+      },
+
+      keyframes: {
+        escalar: {
+          "0% , 100%": { transform: "scale(1)" },
+          "50%": { transform: "scale(0.97)" },
         },
-
-        keyframes: {
-          escalar: {
-            "0% , 100%" : {transform: "scale(1)"},
-            "50%" : {transform: "scale(0.97)"}
-          }
+        burbuja: {
+          "0%, 100%": {
+            borderRadius: "38% 80% 36% 74% / 54% 40% 77% 58%",
+            transform: "translateY(0)",
+          },
+          "50%": {
+            borderRadius: "80% 30% 80% 35% / 34% 40% 65% 89%",
+            transform: "translateY(5%)",
+          },
         },
+      },
 
-        animation:{
-          animacionEscalar:"escalar 2.5s ease-in infinite"
-        },
-        
+      animation: {
+        animacionEscalar: "escalar 2.5s ease-in infinite",
+        burbuja: "burbuja 8s ease-in-out infinite",
+      },
 
-      plugins: [
-        require('tailwindcss-filters'),
-        require('flowbite/plugin'),
-      ],
-    }
+      plugins: [require("tailwindcss-filters"), require("flowbite/plugin")],
+    },
   },
-
-}
-
+};
